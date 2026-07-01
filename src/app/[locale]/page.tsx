2a628737df7b8b0326385d330d73136d32dbe95f
ammar-SoftwareEngineer <ammartolba89@gmail.com>
2026-07-01 12:04:56 +0300

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -27,7 +27,7 @@ export async function generateMetadata({
   const openGraph = seo.meta.open_graph;
   const twitterCard = seo.meta.twitter_card;
   const hreflang = seo.meta.hreflang_tags;
-
+log("metaTags", openGraph);
   return {
     title: metaTags.title,
     description: metaTags.description,
@@ -37,7 +37,7 @@ export async function generateMetadata({
       url: openGraph["og:url"],
       images: [
         {
-          url: openGraph["og:image"],
+          url:"../../../public/logo.jpg",
           alt: metaTags.title,
         },
       ],
